71e282bd9e6603909c6404c6e0f17d801a28e092
Qiwen Zhou <qiwenzhou@Qiwens-MacBook-Pro.local>
2021-03-08 16:45:38 -0800

    mo5class

---
+++ zhou.qiwen/js/app.js
@@ -0,0 +1,41 @@
+
+//Document Ready
+$(()=>{
+
+	checkUserId();
+
+	//console.log("honk")
+
+	$(document)
+
+	/* FORM SUBMITS */
+	.on("submit","#signin-form", function(e){
+		e.preventDefault();
+		checkSigninForm();
+	})
+
+
+	 /* ANCHOR CLICKS */
+   .on("click",".js-logout",function(e){
+      sessionStorage.removeItem('userId');
+      checkUserId();
+   })
+
+
+   /* DATA ACTIVATE */
+   .on("click","[data-activate]",function(e){
+      let target = $(this).data("activate");
+      $(target).addClass("active");
+   })
+
+
+
+
+   $("[data-template]").each(function(){
+      let id = $(this).data("template");
+      let template = $(id).html();
+      $(this).html(template);
+   })
+
+
+});
