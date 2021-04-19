11f0c8596b5bbe41f896bdcbe5362a2cb6f31e50
Qiwen Zhou <qiwenzhou@Qiwens-MBP.attlocal.net>
2021-04-18 17:54:52 -0700

    add json_temp,3 data formats

--- zhou.qiwen/data/json_templates.js
+++ zhou.qiwen/data/json_templates.js
@@ -1,5 +1,69 @@
 
-//
+//USER TEMPLATE
+[
+  '{{repeat(10)}}',
+  {
+    id: '{{index(1)}}',
+  
+    name: '{{firstName()}} {{surname()}}',
+    gender: '{{gender()}}',
+    age: '{{integer(20, 40)}}',
+    phone: '+1 {{phone()}}',
+    address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}',
+    username: function() {
+      return 'user' + this.id;
+  },
+    email: function() {
+      return this.username + '@gmail.com';
+    },
+    password: 'pass',
+    img: function(tags) {
+      return 'https://via.placeholder.com/400/'+tags.integer(700,900) + '/fff/?text=' + this.username;
+    },
+    date_create: '{{date(new Date(2021, 0, 1), new Date(),"YYYY-MM-dd hh:mm:ss")}}'
+  }
+]
+
+
+//ANIMAL DATA
+[
+  '{{repeat(50)}}',
+  {
+    id: '{{index(1)}}',
+    user_id: '{{integer(1,10)}}',
+    
+    name: '{{company()}}',
+    
+    breed: '{{random("seagull","duck","dove")}}',
+    
+    
+    description: '{{lorem(2,"sentences")}}',
+    img:function(tags) {
+      return 'https://via.placeholder.com/400/'+
+        tags.integer(700,999) + '/fff/?text=' + this.name;
+    },
+    date_create: '{{date(new Date(2020, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}'
+  }
+]
+
+//LOCATION DATA
+[
+  '{{repeat(250)}}',
+  {
+    id: '{{index(1)}}',
+    animal_id: '{{integer(1,50)}}',
+    
+    lat: '{{floating(37.787944, 37.761957)}}',
+    lng: '{{floating(-122.524143, -122.340783)}}',
+    
+    description: '{{lorem(3,"sentences")}}',
+    photo: 'https://via.placeholder.com/400/',
+    icon: 'https://via.placeholder.com/400/?text=ICON',
+    date_create: '{{date(new Date(2020, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}'
+  }
+]
+
+
 //Original examples
 [
   '{{repeat(5, 7)}}',
