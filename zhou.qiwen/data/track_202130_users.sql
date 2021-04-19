13adecd9a1ebbac956283cbd82938c49a30778fb
Qiwen Zhou <qiwenzhou@Qiwens-MBP.attlocal.net>
2021-04-18 21:06:56 -0700

    exported SQL files

---
+++ zhou.qiwen/data/track_202130_users.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS `track_202130_users` (
+`id` INT NULL,
+`name` VARCHAR(MAX) NULL,
+`gender` VARCHAR(MAX) NULL,
+`age` INT NULL,
+`phone` VARCHAR(MAX) NULL,
+`address` VARCHAR(MAX) NULL,
+`username` VARCHAR(MAX) NULL,
+`email` VARCHAR(MAX) NULL,
+`password` VARCHAR(MAX) NULL,
+`img` VARCHAR(MAX) NULL,
+`date_create` VARCHAR(MAX) NULL
+);
+
+INSERT INTO track_202130_users VALUES
+(1,'Doris Cohen','female',38,'+1 (837) 501-3309','411 Willoughby Avenue\, Glidden\, South Carolina\, 2027','user1','user1@gmail.com','pass','https://via.placeholder.com/400/702/fff/?text=user1','2021-03-31 06:40:29'),
+(2,'Mccormick Frank','male',23,'+1 (962) 436-2569','165 Eastern Parkway\, Westwood\, Pennsylvania\, 417','user2','user2@gmail.com','pass','https://via.placeholder.com/400/710/fff/?text=user2','2021-02-27 09:38:45'),
+(3,'Concepcion Porter','female',36,'+1 (923) 510-3064','994 Ferris Street\, Faywood\, Kansas\, 1529','user3','user3@gmail.com','pass','https://via.placeholder.com/400/891/fff/?text=user3','2021-02-19 10:02:49'),
+(4,'Noemi Humphrey','female',32,'+1 (834) 532-3810','329 Greene Avenue\, Delshire\, Illinois\, 8267','user4','user4@gmail.com','pass','https://via.placeholder.com/400/748/fff/?text=user4','2021-01-12 06:40:50'),
+(5,'Bean Parrish','male',31,'+1 (936) 444-2563','954 Beayer Place\, Calvary\, Tennessee\, 9136','user5','user5@gmail.com','pass','https://via.placeholder.com/400/874/fff/?text=user5','2021-02-06 05:02:09'),
+(6,'Hoover Wyatt','male',38,'+1 (925) 434-2054','985 McDonald Avenue\, Itmann\, Marshall Islands\, 8095','user6','user6@gmail.com','pass','https://via.placeholder.com/400/822/fff/?text=user6','2021-01-02 04:51:38'),
+(7,'Mclaughlin Oneal','male',31,'+1 (999) 409-2558','558 Livingston Street\, Vincent\, Virginia\, 2802','user7','user7@gmail.com','pass','https://via.placeholder.com/400/886/fff/?text=user7','2021-04-06 10:25:59'),
+(8,'Cleveland Branch','male',31,'+1 (896) 556-3097','803 Lacon Court\, Twilight\, Washington\, 7409','user8','user8@gmail.com','pass','https://via.placeholder.com/400/818/fff/?text=user8','2021-01-09 03:58:27'),
+(9,'Hope Reilly','female',35,'+1 (995) 547-3364','606 Guider Avenue\, Franklin\, New Hampshire\, 5885','user9','user9@gmail.com','pass','https://via.placeholder.com/400/856/fff/?text=user9','2021-02-03 06:45:35'),
+(10,'Serrano Allison','male',24,'+1 (820) 599-3473','686 Henry Street\, Coultervillle\, District Of Columbia\, 8108','user10','user10@gmail.com','pass','https://via.placeholder.com/400/786/fff/?text=user10','2021-01-06 03:39:16');
