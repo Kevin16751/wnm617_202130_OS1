fd3c3c1aaa5f863c03021bd0a9f8bcd091ddfcc1
Qiwen Zhou <qiwenzhou@Qiwens-MBP.attlocal.net>
2021-05-03 13:17:50 -0700

    map markers added

--- zhou.qiwen/js/maps.js
+++ zhou.qiwen/js/maps.js
@@ -45,4 +45,79 @@ const makeMarkers = (map_el,map_locs) => {
    });
 
    map_el.data("markers",markers);
+   setTimeout(()=>{setMapBounds(map_el,map_locs)},150);
 }
+
+
+const setMapBounds = (map_el,map_locs) => {
+   let map = map_el.data("map");
+   let zoom = 14;
+
+   if(map_locs.length==1) {
+      map.setCenter(map_locs[0]);
+      map.setZoom(zoom);
+   } else if(map_locs.length==0) {
+      if(window.location.protocol!=="https:") return;
+      else {
+         navigator.geolocation.getCurrentPosition(p=>{
+            let pos = {
+               lat:p.coords.latitude,
+               lat:p.coords.longitude
+            };
+            map.setCenter(pos);
+            map.setZoom(zoom);
+         },(...args)=>{
+            console.log(args)
+         },{
+            enableHighAccuracy:false,
+            timeout:5000,
+            maximumAge:0
+         })
+      }
+   } else {
+      let bounds = new google.maps.LatLngBounds(null);
+      map_locs.forEach(o=>{
+         bounds.extend(o);
+      });
+      map.fitBounds(bounds);
+   }
+}
+
+
+
+
+
+
+let mapStyles = [
+    {
+        "stylers": [
+            {
+                "hue": "#2c3e50"
+            },
+            {
+                "saturation": 250
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 50
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+];
--- zhou.qiwen/js/pages.js
+++ zhou.qiwen/js/pages.js
@@ -1,5 +1,43 @@
 
-const RecentPage = async () => {}
+const RecentPage = async () => {
+   let locations = await query({
+      type:'recent_locations',
+      params:[sessionStorage.userId]
+   });
+   console.log(locations)
+
+   let valid_animals = locations.result.reduce((r,o)=>{
+      o.icon = o.img;
+      if(o.lat && o.lng) r.push(o);
+      return r;
+   },[]);
+
+   let map_el = await makeMap("#recent-page .map");
+   makeMarkers(map_el,valid_animals);
+
+   map_el.data("markers").forEach((o,i)=>{
+      o.addListener("click",function(){
+
+         /* SIMPLE EXAMPLE */
+         /*sessionStorage.animalId = valid_animals[i].animal_id;
+         $.mobile.navigate("#animal-profile-page");*/
+
+         /* INFOWINDOW EXAMPLE */
+         /*map_el.data("infoWindow")
+            .open(map_el.data("map"),o)
+         map_el.data("infoWindow")
+            .setContent(makeAnimalPopup(valid_animals[i]))*/
+
+         /* ACTIVATE EXAMPLE */
+         $("#recent-drawer")
+            .addClass("active")
+            .find(".modal-body")
+            .html(makeAnimalPopup(valid_animals[i]))
+      })
+   })
+}
+
+
 
 const ListPage = async () => {
    let animals = await query({
@@ -16,6 +54,9 @@ const ListPage = async () => {
    $("#list-page .animallist").html(animal_template);
 }
 
+
+
+
 const UserProfilePage = async () => {
    let user = await query({
       type:'user_by_id',
@@ -26,22 +67,73 @@ const UserProfilePage = async () => {
       .html(makeUserProfile(user.result[0]));
 }
 
+const UserEditPage = async () => {
+   let user = await query({
+      type:'user_by_id',
+      params:[sessionStorage.userId]
+   });
+
+   $("#user-edit-form")
+         .html(makeUserProfileUpdateForm(user.result[0]));
+}
+
+const UserPasswordPage = async () => {
+   let user = await query({
+      type:'user_by_id',
+      params:[sessionStorage.userId]
+   });
+
+   $("#user-password-form")
+         .html(makeUserPasswordUpdateForm(user.result[0]));
+}
+
+
+
 const AnimalProfilePage = async () => {
-   let r = await query({
+   query({
+      type:'animal_by_id',
+      params:[sessionStorage.animalId]
+   }).then(r=>{
+      let animal = r.result[0];
+      console.log("ANIMAL DATA", animal)
+
+      if(!$("#animal-profile-page .active").length) {
+         $("#animal-profile-page .animal-nav li:first-child").addClass("active")
+         $("#animal-profile-page .animal-bottom-section:first-child").addClass("active")
+      }
+
+      $("#animal-profile-page .animal-top")
+         .css({backgroundImage:`url(${animal.img})`})
+      $("#animal-profile-page .animal-info")
+         .html(makeAnimalInfo(animal));
+   });
+   
+
+   query({
+      type:'locations_by_animal_id',
+      params:[sessionStorage.animalId]
+   }).then(async (r)=>{
+      console.log("ANIMAL LOCATIONS", r.result)
+      let map_el = await makeMap("#animal-profile-page .map");
+      makeMarkers(map_el,r.result)
+   });
+}
+
+const AnimalEditPage = async () => {
+   let animal = await query({
       type:'animal_by_id',
       params:[sessionStorage.animalId]
    });
-   let animal = r.result[0];
-   console.log(animal)
 
-   if(!$("#animal-profile-page .active").length) {
-      $("#animal-profile-page .animal-nav li:first-child").addClass("active")
-      $("#animal-profile-page .animal-bottom-section:first-child").addClass("active")
-   }
+   $("#animal-edit-form")
+         .html(makeAnimalProfileUpdateForm(animal.result[0]));
+}
 
-   $("#animal-profile-page .animal-top")
-      .css({backgroundImage:`url(${animal.img})`})
-   $("#animal-profile-page .animal-info")
-      .html(makeAnimalInfo(animal));
 
+
+
+
+const ChooseLocationPage = async () => {
+   let map_el = await makeMap("#choose-location-page .map");
+   makeMarkers(map_el,[])
 }
